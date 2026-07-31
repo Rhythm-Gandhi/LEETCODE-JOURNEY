5a877d6b217e4a5c9e95cf2171e6ccbc64c31cd6
Rhythm-Gandhi <rhythmgandhi2005@gmail.com>
2026-08-01 01:20:24 +0530

    Time: 99 ms (67.03%), Space: 20 MB (53.79%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,21 +1,9 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        freq = [0] * 26
-
-        # Count frequency of each letter
-        for ch in word:
-            freq[ord(ch) - ord('a')] += 1
-
-        # Sort frequencies in descending order
-        freq.sort(reverse=True)
+        freq = sorted(Counter(word).values(), reverse=True)
 
         ans = 0
-
-        # Assign costs greedily
-        for i in range(26):
-            if freq[i] == 0:
-                break
-            cost = i // 8 + 1
-            ans += freq[i] * cost
+        for i, f in enumerate(freq):
+            ans += f * (i // 8 + 1)
 
         return ans
