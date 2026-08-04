0a850bb7311d1b69aa94239cc20e4e2aa2c63513
Rhythm-Gandhi <rhythmgandhi2005@gmail.com>
2026-08-05 00:55:25 +0530

    Time: 1 ms (79.38%), Space: 19.4 MB (18.85%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -1,10 +1,9 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
-        n = sorted(nums)
-        sett  = set()
-        for i in range(n[0],n[-1]):
-            if i not in n:
-                sett.add(i)
-            else:
-                continue
-        return sorted(list(sett))
+        mn, mx = min(nums), max(nums)
+        s = set(nums)
+        lst = []
+        for i in range(mn+1,mx):
+            if i not in s:
+               lst.append(i)
+        return lst
