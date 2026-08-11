4fddcbe64b1525aa4e4cb4e2d879907bcd27c57b
Rhythm-Gandhi <rhythmgandhi2005@gmail.com>
2026-08-12 02:22:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.6%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -1,16 +1,15 @@
 class Solution:
     def missingInteger(self, A: list[int]) -> int:
         n = len(A)
-        seen = set(A)
+        sen = set(A)
         sum = A[0]
-
-        for i in range(1, n):
-            if A[i] == A[i - 1] + 1:
-                sum += A[i]
+        for j in range(1,n):
+            if A[j] == A[j-1]+1:
+                sum+=A[j]
             else:
                 break
 
-        while sum in seen:
-            sum += 1
+        while sum in A:
+            sum +=1
 
         return sum
