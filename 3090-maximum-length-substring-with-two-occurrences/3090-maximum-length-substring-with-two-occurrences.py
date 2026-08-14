75507af44225618686099a3431175203f3f3a08b
Rhythm-Gandhi <rhythmgandhi2005@gmail.com>
2026-08-15 01:29:39 +0530

    Time: 5 ms (36.44%), Space: 19.3 MB (60.15%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
@@ -1,18 +1,21 @@
+from collections import defaultdict
 class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
-        frq = [0]*26
+        #frq = [0]*26
+        frq = defaultdict(int)
         j= 0
         maxi = 0
         i = 0
         #for i in range(len(s)):
         while i<len(s):
-            ind = ord(s[i])-ord('a')
-            frq[ind] +=1
+            #ind = ord(s[i])-ord('a')
+            frq[s[i]] +=1
                 
-            while frq[ind]>2:
-                frq[ord(s[j])-ord('a')] -=1
+            while frq[s[i]]>2:
+                #frq[ord(s[j])-ord('a')] -=1
+                frq[s[j]] -=  1
                 j +=1
 
             maxi = max(maxi,i-j+1)
-            i+=1
+            i+= 1
         return maxi
